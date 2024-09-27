fa9ca75b8d10c80b3cefc923fecc7ef98aa39cf6
Ricardo Cavalier <ricavalier@hotmail.com>
2024-09-27 17:33:37 -0300

    aula 04

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express'
 const app = express()
 
+// indicar para ler o corpo como json
+app.use(express.json())
+
 //mock
 const selecoes = [
     { id: 1, selecao: 'Brasil', grupo: 'G'},
@@ -9,11 +12,32 @@ const selecoes = [
     { id: 4, selecao: 'Camarões', grupo: 'G'},
 ]
 
+const buscarSelecaoId = (id) => {
+  return selecoes.find(obj => obj.id == id)
+}
+const buscarIndexSelecao = (id) => {
+  return selecoes.findIndex(obj => obj.id == id)
+}
+
 app.get('/', (req, res) => {
   res.send('Novo teste!')
 })
 app.get('/selecoes', (req, res) => {
   res.status(200).send(selecoes)
+})
+app.get('/selecoes/:id', (req, res) => {
+  // const index = req.params.id
+  res.status(200).json(buscarSelecaoId(req.params.id))
+})
+app.delete('/selecoes/:id', (req, res) => {
+  const index = buscarIndexSelecao(req.params.id)
+  console.log('Index', index)
+  selecoes.splice(index, 1)
+  res.status(200).json(selecoes)
+})
+app.post('/selecoes', (req, res) => {
+    selecoes.push(req.body)
+    res.status(201).send('Seleção cadastrada com sucesso!')
 })
 
 export default app
