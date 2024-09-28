14c81584ec277e6a3bef39c7947d016843a2b6d5
Ricardo Cavalier <ricavalier@hotmail.com>
2024-09-27 21:05:42 -0300

    Aula 06

---
+++ infra/conexao.js
@@ -0,0 +1,10 @@
+import mysql from 'mysql'
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: '3001',
+    database: 'bdcopa'
+})
+
+export default conexao
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 import app from './src/app.js'
 const port = 3000
 
-app.listen(port, () => {
-    console.log(`Servidor rodando na no endereço: http://localhost:${port}`)
+import conexao from './infra/conexao.js'
+//fazer a conexao
+conexao.connect((err) => {
+    if(err) {
+        console.log(err)
+    } else {
+        console.log('Conexão realizada com sucesso!')
+        app.listen(port, () => {
+            console.log(`Servidor rodando na no endereço: http://localhost:${port}`)
+        })
+    }
 })
